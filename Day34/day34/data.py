c665a6a357d83b41478fb656a0804379d7eb22d6
Chyarpsh <chowdhurya290@gmail.com>
2024-06-22 00:41:51 -0500

    main

--- Day34/day34/data.py
+++ Day34/day34/data.py
@@ -2,8 +2,9 @@ import requests
 
 parameters = {
     "amount": 10,
-    "Type": "boolean"
+    "type": "boolean",
 }
+
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
